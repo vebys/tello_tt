7a5b230c0e37472a84edfa829d5776793363e8e3
hp <hp@qq.com>
2022-02-18 19:21:42 +0800

    update main

--- main.py
+++ main.py
@@ -1,10 +1,60 @@
 from tello_sdk_stand import *
-from yolov5.detect import DetectApi
-from yolov5.tt_api import get_qi_info
+from yolov5master.detect import DetectApi
+from yolov5master.tt_api import get_qi_info,get_quan_info
 import time, sys
 
 model = DetectApi(weights=['.\\weights\\best.pt'], nosave=False)
 dj = Start()  # 初始化飞机对象
+
+# try:
+#     dj.take_off()  # 起飞
+#     dj.up(40)
+#     # time.sleep(5)
+#
+#     # dj.take_photo()
+#     time.sleep(2)
+#     img_path = dj.take_photo(num=2)
+#     print('图片路径：',img_path)
+#     qi_info = get_qi_info(model,img_path)
+#     print(qi_info)
+#     if qi_info['code'] == 'action':
+#         dj.fly(direction=qi_info['direction'],distance=qi_info['distance'])
+#         if not qi_info['finish']:
+#             img_path =dj.take_photo(num=2)
+#             qi_info = get_qi_info(model, img_path)
+#             dj.fly(direction=qi_info['direction'], distance=qi_info['distance'])
+#             print('需要再次调用拍照定位')
+#     elif qi_info['code'] == 'no action':
+#         print('无需调整位置')
+#     else:
+#         # code 为err
+#         if not qi_info['finish']:
+#             print('未检测到旗子，想右 向后飞行后再试')
+#             dj.back(40)
+#             dj.left(40)
+#             img_path = dj.take_photo(num=2)
+#             print('图片路径：', img_path)
+#             qi_info = get_qi_info(model, img_path)
+#             print(qi_info)
+#             if qi_info['code'] == 'action':
+#                 dj.fly(direction=qi_info['direction'], distance=qi_info['distance'])
+#                 if not qi_info['finish']:
+#                     img_path = dj.take_photo(num=2)
+#                     qi_info = get_qi_info(model, img_path)
+#                     dj.fly(direction=qi_info['direction'], distance=qi_info['distance'])
+#         else:
+#             print('代码有bug:',qi_info['code'])
+#
+#     dj.take_photo(num=1)
+#     time.sleep(1)
+#     # dj.take_photo()
+#     dj.land()  # 降落
+# except Exception as e:
+#     print('飞行异常准备降落，错误代码：', e)
+#     dj.land()  # 降落
+# finally:
+#     dj.close()  # 释放飞机资源
+#     sys.exit()
 
 try:
     dj.take_off()  # 起飞
@@ -15,13 +65,13 @@ try:
     time.sleep(2)
     img_path = dj.take_photo(num=2)
     print('图片路径：',img_path)
-    qi_info = get_qi_info(model,img_path)
+    qi_info = get_quan_info(model,img_path)
     print(qi_info)
     if qi_info['code'] == 'action':
         dj.fly(direction=qi_info['direction'],distance=qi_info['distance'])
         if not qi_info['finish']:
             img_path =dj.take_photo(num=2)
-            qi_info = get_qi_info(model, img_path)
+            qi_info = get_quan_info(model, img_path)
             dj.fly(direction=qi_info['direction'], distance=qi_info['distance'])
             print('需要再次调用拍照定位')
     elif qi_info['code'] == 'no action':
@@ -34,13 +84,13 @@ try:
             dj.left(40)
             img_path = dj.take_photo(num=2)
             print('图片路径：', img_path)
-            qi_info = get_qi_info(model, img_path)
+            qi_info = get_quan_info(model, img_path)
             print(qi_info)
             if qi_info['code'] == 'action':
                 dj.fly(direction=qi_info['direction'], distance=qi_info['distance'])
                 if not qi_info['finish']:
                     img_path = dj.take_photo(num=2)
-                    qi_info = get_qi_info(model, img_path)
+                    qi_info = get_quan_info(model, img_path)
                     dj.fly(direction=qi_info['direction'], distance=qi_info['distance'])
         else:
             print('代码有bug:',qi_info['code'])
--- yolov5_new/tt_api.py
+++ yolov5_new/tt_api.py
@@ -4,6 +4,7 @@ import time , operator
 model = DetectApi(weights=['weights\\best.pt'], nosave=False)
 
 
+
 def get_qi_info(model,img_path):
     result = dict()
     res1 = model.detect(source=img_path,conf_thres=0.2)
@@ -27,6 +28,7 @@ def get_qi_info(model,img_path):
             break
     try:
         if qi:
+            """w=30"""
             # f = (p*d)/w #f:焦距，p实物在图片中的像素，d:摄像头距离实物的距离，w:物体实际宽度cm
             f = 1020.47
             # dis = (w*f)/p1 # w:物体实际宽度cm ，f：焦距, p1物体在图片中的像素宽度
@@ -71,6 +73,86 @@ def get_qi_info(model,img_path):
             result['code'] = 'err'
             result['finish'] = False
             result['msg'] = '未识别到旗'
+    except Exception as e:
+        print(e)
+        result['code'] = 'err'
+        result['msg'] = str(e)
+        result['finish'] = True
+
+    return result
+
+
+
+def get_quan_info(model,img_path):
+    result = dict()
+    res1 = model.detect(source=img_path,conf_thres=0.2)
+    # res1 = model.detect(source='..\\tello_tt_yolov5\\img\\1.2.jpg',conf_thres=0.4)
+    # res2 = model.detect(source='..\\tello_tt_yolov5\\img\\1.2-2.jpg',conf_thres=0.4)
+    # res3 = model.detect(source='..\\tello_tt_yolov5\\img\\1.3-1.jpg',conf_thres=0.4)
+    # res4 = model.detect(source='..\\tello_tt_yolov5\\img\\1.3-2.jpg',conf_thres=0.4)
+    # res5 = model.detect(source='..\\tello_tt_yolov5\\img\\1.3-3.jpg',conf_thres=0.4)
+    print(res1)
+    # print(res2)
+    # print(res3)
+    # print(res4)
+    # print(res5)
+
+    t3 = time.time()
+    res = sorted(res1, key=operator.itemgetter('conf'), reverse=True)
+    print(res)
+    # print(res)
+    quan = {}
+    for r in res:
+        if r['label'] == 'quan':
+            quan = r
+            break
+    try:
+        if quan:
+            """w=48"""
+            # f = (p*d)/w #f:焦距，p实物在图片中的像素，d:摄像头距离实物的距离，w:物体实际宽度cm
+            f = 391.67
+            # dis = (w*f)/p1 # w:物体实际宽度cm ，f：焦距, p1物体在图片中的像素宽度
+            dis = (48*f)/(quan['x2y2'][0]-quan['x1y1'][0])
+            result['dis_forward'] = dis # 距离圈的距离
+            dis_cm = 48/(quan['x2y2'][0]-quan['x1y1'][0]) # 每像素多少厘米，需要用相似三角形计算
+            if int(quan['x1y1'][0]) < 10:
+                result['code'] = 'action'
+                result['direction'] = 'left'
+                result['distance'] = 30
+                result['finish'] = False
+                result['msg'] ='圈在左边界，测量可能不准确，需向 左  飞行后重新拍照计算'
+                return result
+            elif int(quan['src_img_size'][1]) == (quan['x2y2'][0]):
+                result['code'] = 'action'
+                result['direction'] = 'right'
+                result['distance'] = 30
+                result['finish'] = False
+                result['msg'] ='圈在右边界，测量可能不准确，需向  右  飞行重新拍照计算'
+                return result
+            diff = (quan['src_img_size'][1]-quan['x2y2'][0]-quan['x1y1'][0])/2
+            diff = int(diff*dis_cm)
+            print('diff:: ',diff)
+            if abs(diff)<20:
+                result['finish'] = True
+                result['code'] = 'no action'
+                result['msg'] = '偏移居里路小于20，无需调整位置'
+            else:
+                if diff>0:
+                    result['direction'] = 'left'
+                    result['msg'] = f'需向左飞{diff}'
+                    # print('向左飞')
+                else:
+                    result['direction'] = 'right'
+                    result['msg'] = f'需向右飞{diff}'
+                result['code'] = 'action'
+                result['distance'] = abs(diff)
+                result['finish'] = True
+                    # print('向右飞')
+        else:
+            print('未识别到圈')
+            result['code'] = 'err'
+            result['finish'] = False
+            result['msg'] = '未识别到圈'
     except Exception as e:
         print(e)
         result['code'] = 'err'
