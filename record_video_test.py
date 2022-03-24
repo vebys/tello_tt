5e646446202d8eff4cacb2e4d5f1e41d09b9276c
hp <hp@qq.com>
2022-03-24 21:47:15 +0800

    增加杆子定位

--- record_video_test.py
+++ record_video_test.py
@@ -14,12 +14,14 @@ import cv2
 
 dj = Start()
 # try:
-video_writer = cv2.VideoWriter('img/video/xx.avi', cv2.VideoWriter_fourcc(*'XVID'), 20.0, (960, 720))
-for i in range(0,20):
+video_writer = cv2.VideoWriter('img/video/xx.avi', cv2.VideoWriter_fourcc(*'XVID'), 15.0, (960, 720))
+for i in range(0,100):
     video_writer.write(dj.camera_obj.read_video_frame())
-    time.sleep(1)
+    time.sleep(0.1)
+    print(i)
 video_writer.release()
-os.system(f'ffmpeg -i "img/video/xx.avi" -vcodec h264 "img/video/xx.mp4"')
+# os.system(f'ffmpeg -i "img/video/xx.avi" -vcodec h264 "img/video/xx.mp4"')
 dj.camera_obj.stop_video_stream()
 # except Exception as e:
 #     print(e)
+dj.close()
