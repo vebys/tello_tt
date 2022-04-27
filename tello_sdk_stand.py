adbae26375dc27c4183659cd434d9c651c64591c
hp <hp@qq.com>
2022-04-27 17:40:46 +0800

    新场地代码

--- tello_sdk_stand.py
+++ tello_sdk_stand.py
@@ -1,6 +1,6 @@
 # -*-coding:utf-8-*-
 # Copyright (c) 2020 DJI.
-# version 2022-20
+# version 2022-4-27
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
